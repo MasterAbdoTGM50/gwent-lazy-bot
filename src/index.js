b77df2f3ea0f94532162b120a1146f475132e743
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2019-10-09 18:43:28 +0200

    I should have made separate commits but oh well

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const Fuse = require("fuse.js");
 
 const config = require("./config");
+const cards = require("./cards.json");
 
-client.once("ready", () => { console.log("GWENT Lazy Bot reporting for duty"); });
+let nameToIdMap = [];
+let fuse;
 
-client.on('message', message => {
-    console.log(message.content);
+client.once("ready", () => {
+    console.log("GWENT Lazy Bot reporting for duty");
+    Object.values(cards).forEach((card) => {
+        nameToIdMap.push({ id: card["ingameId"], name: card["name"]["en-US"] });
+    });
+
+    const options = {
+        shouldSort: true,
+        threshold: 0.25,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "name"
+        ]
+    };
+    fuse = new Fuse(nameToIdMap, options);
+});
+
+client.on("message", message => {
+    let regex = /\[(.*?)]/g;
+
+    let matches = [], match;
+    while ((match=regex.exec(message)) !== null) {
+        matches.push(match[1]);
+    }
+
+    matches.forEach((match) => {
+            let result = fuse.search(match);
+            if(result.length !== 0) {
+                message.channel.send("https://gwent.one/en/card/" + result[0].id);
+            }
+    });
+
 });
 
 client.login(config.token);
