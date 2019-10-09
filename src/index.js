cbe50d32e7dd0a45044f8ade31b89c35581d7b73
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2019-10-09 21:00:28 +0200

    Bot now fetches card name data from gwent.one instead of a local json file

--- src/index.js
+++ src/index.js
@@ -1,48 +1,63 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const Fuse = require("fuse.js");
 
+const axios = require("axios");
+const client = new Discord.Client();
 const config = require("./config");
-const cards = require("./cards.json");
 
-let nameToIdMap = [];
+let cards = [];
 let fuse;
 
 client.once("ready", () => {
-    console.log("GWENT Lazy Bot reporting for duty");
-    Object.values(cards).forEach((card) => {
-        nameToIdMap.push({ id: card["ingameId"], name: card["name"]["en-US"] });
-    });
-
-    const options = {
-        shouldSort: true,
-        threshold: 0.25,
-        location: 0,
-        distance: 100,
-        maxPatternLength: 32,
-        minMatchCharLength: 1,
-        keys: [
-            "name"
-        ]
-    };
-    fuse = new Fuse(nameToIdMap, options);
+    updateCards();
+    console.log("GWENT Lazy Bot reporting for duty!");
 });
 
+function updateCards() {
+    axios.get("https://gwent.one/cardbot").then(res => {
+        let _cards = [];
+        Object.values(res.data).forEach((card) => {
+            _cards.push(card);
+        });
+        cards = _cards;
+
+        const options = {
+            shouldSort: true,
+            threshold: 0.25,
+            location: 0,
+            distance: 100,
+            maxPatternLength: 32,
+            minMatchCharLength: 1,
+            keys: [
+                "name"
+            ]
+        };
+
+        fuse = new Fuse(cards, options);
+
+        console.log("GWENT Lazy Bot 'updated' for duty!");
+    });
+}
+
 client.on("message", message => {
-    let regex = /\[(.*?)]/g;
 
-    let matches = [], match;
-    while ((match=regex.exec(message)) !== null) {
-        matches.push(match[1]);
-    }
+    if(message.content === "!lazy_update") {
+        updateCards();
+    } else {
+        let regex = /\[(.*?)]/g;
 
-    matches.forEach((match) => {
+        let matches = [], match;
+        while ((match=regex.exec(message.content)) !== null) {
+            matches.push(match[1]);
+        }
+
+        matches.forEach((match) => {
             let result = fuse.search(match);
             if(result.length !== 0) {
                 message.channel.send("https://gwent.one/en/card/" + result[0].id);
             }
-    });
-
+        });
+    }
 });
 
 client.login(config.token);
