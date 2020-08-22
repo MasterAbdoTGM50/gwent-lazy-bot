007b8e94ed1df27057db29659c6fe92385afde7f
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2020-08-22 22:35:47 +0200

    Tweak card embed to better fit leader abilities

--- src/actions/card.js
+++ src/actions/card.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js");
+const utils = require("./../utils");
 
 function buildCardEmbed(bot, card, locale) {
     let embed = new Discord.MessageEmbed();
-    let title = card.name[locale] + " - " + card.categories[locale];
+    let title = card.name[locale];
+    if(card.categories[locale] !== "") { title += " - " + card.categories[locale]; }
     let url = "https://gwent.one/en/card/" + card.id;
     let thumb = "https://gwent.one/img/assets/medium/art/" + card.art + ".jpg";
-    let faction = bot.translations[locale]["factions"][card.factions[0]];
+    if(card.type === "leader") { thumb = "https://gwent.one/img/icon/ability/" + card.id + ".png"}
     let color = bot.lib.colors[card.factions[0]];
+    let faction = bot.translations[locale]["factions"][card.factions[0]];
     if(card.factions[1] !== "") { faction += " & " + bot.translations[locale]["factions"][card.factions[1]] }
     let rarity = bot.translations[locale]["rarities"][card.rarity];
     let type = bot.translations[locale]["types"][card.type];
@@ -20,10 +23,13 @@ function buildCardEmbed(bot, card, locale) {
     embed.setTitle(title);
     embed.setURL(url);
     embed.setThumbnail(thumb);
-    embed.setAuthor(faction + " - " + rarity + " - " + type)
+    if(card.type !== "leader") {
+        embed.addField("\u200b", stats);
+        embed.addField("\u200b","*" +  flavor + "*");
+        faction += " - " + rarity + " " + type;
+    }
+    embed.setAuthor(faction)
     embed.setDescription(ability);
-    embed.addField("\u200b", stats);
-    embed.addField("\u200b","*" +  flavor + "*");
     return embed;
 }
 
