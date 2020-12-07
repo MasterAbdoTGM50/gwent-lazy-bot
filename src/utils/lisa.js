be6200196d4cc41aab46511da2f5740b30e888cd
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2020-12-07 15:42:20 +0200

    Rework LISA's filtering process to not hard wire into array indices

--- src/utils/lisa.js
+++ src/utils/lisa.js
@@ -1,49 +1,61 @@
 class Lisa {
+
     #keyPath;
+    #aliasPath;
     #rules = [];
+    #entries;
     #filters = [
-        function (rule, key) { return rule.key.includes(key) },
-        function (rule, key) { return rule.key === key; }
+        function (rule, alias) { return rule.alias.includes(alias) },
+        function (rule, alias) { return rule.alias.startsWith(alias) },
+        function (rule, alias) { return rule.alias.endsWith(alias) },
+        function (rule, alias) { return rule.alias === alias; }
     ];
 
-    constructor(entries, keyPath) {
+    constructor(entries, keyPath, aliasPath) {
+        this.#entries = entries;
         this.#keyPath = keyPath;
-        this.#buildRules(entries);
+        this.#aliasPath = aliasPath;
+        this.#buildRules(this.#entries);
     }
 
+    #resolveObjProperty = (obj, path) => { return path.split(".").reduce((o, p) => o && o[p] || null, obj); }
+
     #buildRules = entries => {
-        for(let i = 0; i < entries.length; ++i) {
-            let words = this.#keyPath.split(".").reduce((o, p) => o && o[p] || null, entries[i]).split(/\s+/);
-            for(let word of words) {
-                this.#rules.push({ key: word.toLowerCase(), index: i });
+        for(let entry of entries) {
+            let key = this.#resolveObjProperty(entry, this.#keyPath);
+            let alias = this.#resolveObjProperty(entry, this.#aliasPath).split(/\s+/);
+            for(let word of alias) {
+                this.#rules.push({ key: key , alias: word.toLowerCase() });
             }
         }
     };
 
-    search(key) {
-        let keys = key.toLowerCase().split(/\s+/).map(key => key.trim());
+    findByKey(key) { return this.#entries.find(entry => this.#resolveObjProperty(entry, this.#keyPath) === key); }
 
-        let attempt = this.#filterRules(keys, this.#filters[0]);
+    findByAlias(alias) {
+        let aliases = alias.toLowerCase().split(/\s+/).map(alias => alias.trim());
+
+        let attempt = this.#filterRules(aliases, this.#filters[0]);
         let result = attempt;
         for(let i = 1; i < this.#filters.length; ++i) {
-            attempt = this.#filterRules(keys, this.#filters[i]);
+            attempt = this.#filterRules(aliases, this.#filters[i]);
             if(attempt.length !== 0 && attempt.length < result.length) { result = attempt; }
             if(attempt.length === 1) { break; }
         }
 
-        return result;
+        return result.map(key => this.findByKey(key));
     }
 
-    #filterRules = (keys, filterFn) => {
+    #filterRules = (aliases, filterFn) => {
         let matches = [];
 
-        keys.forEach(key => {
+        aliases.forEach(alias => {
             this.#rules.forEach(rule => {
-                if(filterFn(rule, key)) { matches.push(rule); }
+                if(filterFn(rule, alias)) { matches.push(rule); }
             })
         })
 
-        let occurrences = matches.reduce((acc, rule) => acc.set(rule.index, (acc.get(rule.index) || 0) + 1), new Map());
+        let occurrences = matches.reduce((acc, rule) => acc.set(rule.key, (acc.get(rule.key) || 0) + 1), new Map());
         let mostOccurrence = Math.max(...occurrences.values());
 
         return [...occurrences.entries()].filter(([, v]) => v === mostOccurrence).map(([k,]) => k);
