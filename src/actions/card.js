17110211448190c4ea1f257e23c973ea472978c9
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2020-12-09 15:08:48 +0200

    reddit...

--- src/actions/card.js
+++ src/actions/card.js
@@ -33,6 +33,23 @@ function buildCardEmbed(bot, card, locale) {
     return embed;
 }
 
+function buildMemeCardEmbed(bot, card, locale) {
+    let embed = new Discord.MessageEmbed();
+    let title = card.name[locale];
+    let url = "https://gwent.one/" + locale + "/card/" + card.id;
+    let thumb = "https://gwent.one/img/assets/medium/art/" + card.art + ".jpg";
+    if(card.type === "leader") { thumb = "https://gwent.one/img/icon/ability/" + card.id + ".png" }
+    let color = bot.lib.colors[card.factions[0]];
+    let flavor = card.flavor[locale].replace(/\\n/g, "\n");
+
+    embed.setColor(color);
+    embed.setTitle(title);
+    embed.setURL(url);
+    embed.setThumbnail(thumb);
+    embed.setDescription(flavor);
+    return embed;
+}
+
 module.exports = {
     async handle(bot, message, locale) {
         let matches = utils.findMatches(message.content, /\[(.*?)]/g);
@@ -41,7 +58,10 @@ module.exports = {
 
             let result = bot.nicknames.filter(nick => { return nick.name === match.toLowerCase() });
             if(result.length === 1) {
-                message.channel.send(buildCardEmbed(bot, bot.lisas[locale].findByKey(result[0].id), locale));
+                if(match === "reddit") { message.channel.send(buildMemeCardEmbed(bot, bot.lisas[locale].findByKey(result[0].id), locale)); }
+                else {
+                    message.channel.send(buildCardEmbed(bot, bot.lisas[locale].findByKey(result[0].id), locale));
+                }
                 return;
             }
 
